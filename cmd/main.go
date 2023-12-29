64668563c2933159a2125ce87eee98929127cf2c
jenovax86 <shruzvn@gmail.com>
2023-12-30 03:20:40 +0330

    feat: add bmi-calculator

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
 
+func bmiCalculater() float64 {
+	var weight float64
+	var height float64
+	fmt.Println("Enter your weight and your height to calculate your BMI(body mass index)")
 
-func main(){
+	fmt.Scan(&weight, &height)
+	finalHeight := math.Pow(height/100, 2)
+	result := weight / finalHeight
+	return result
+}
 
+func main() {
+	fmt.Println(bmiCalculater())
 }
